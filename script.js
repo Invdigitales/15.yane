93da7a776b96c29daa3ec2ccf1a647effe6fc1a9
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2025-04-18 18:27:25 -0300

    cinco

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ function diaEventoAhora () {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3263.70794313738!2d-60.460772000000006!3d-35.114003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzXCsDA2JzUwLjQiUyA2MMKwMjcnMzguOCJX!5e0!3m2!1ses-419!2sar!4v1736426200477!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3340.227027882839!2d-62.85029419999999!3d-33.1556662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95cecb67fc1daf7f%3A0xd5264f71a8a4edf8!2sVilco!5e0!3m2!1ses-419!2sar!4v1745010394038!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
         'Podes ampliar el mapa',
         
         
